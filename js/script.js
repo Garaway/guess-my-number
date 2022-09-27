d34bc325d91e8c09fe02d120864f6a40fe33f5f2
ninjavsrabbit <ninjavsrabbit@gmail.com>
2022-09-27 20:48:21 +0300

    added js code

---
+++ js/script.js
@@ -0,0 +1,58 @@
+
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+document.querySelector('.header__number').textContent = secretNumber;
+
+let score = 20;
+let highscore = 0;
+
+function displayMessage(message) {
+    document.querySelector('.main__message').textContent = message;
+}
+
+document.querySelector('.main__check').addEventListener('click', function () {
+
+    let guess = Number(document.querySelector('.main__guess').value);
+
+    if (!guess) {
+        displayMessage('No number');
+        return;
+    }
+
+    if (score > 1) {
+        score--;
+        document.querySelector('.score').textContent = score;
+        displayMessage(guess > secretNumber ? 'Too high' : 'To low');
+    }
+
+    if (guess === secretNumber) {
+        displayMessage('Correct Number');
+        document.querySelector('body').style.backgroundColor = '#00b400';
+        document.querySelector('.header__number').style.width = '20rem';
+        if (score > highscore) {
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore + 1;
+        }
+        return;
+    }
+
+    if (score === 0) {
+        displayMessage('Loh');
+        return;
+    }
+})
+
+const funct = () => {
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    document.querySelector('body').style.backgroundColor = '#222222';
+    document.querySelector('.header__number').style.width = '10rem';
+    displayMessage('Start guessing...');
+    console.log(document.querySelector('.header__guess'));
+    document.querySelector('.main__guess').value = '';
+
+    document.querySelector('.header__number').textContent = secretNumber;
+};
+document.querySelector('.header__again').addEventListener('click', funct)
+
